6998b077f4f9249f2bfd2e475779ee33ed2f408f
Elint Eerie <96844092+elinteerie@users.noreply.github.com>
2023-03-26 03:31:01 +0100

    Update settings.py
    added web-production-a455.up.railway.app

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-30qau++f8#$ehhun%#yv09ox27u)odvc)z=%t1nmkq!ir@k-)b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1", "web-production-a455.up.railway.app"]
 
 
 # Application definition
